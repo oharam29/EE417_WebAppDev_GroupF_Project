8ae7b65e0adc2eb883a9679a83f64228705ae751
ciaranius <78697615+ciaranius@users.noreply.github.com>
2021-04-06 12:14:18 +0100

    Update LoginServlet.java
    Updated Login Servlet. Changed form field names to match login.jsp. Added conditional user/admin launch

--- GFB/src/LoginServlet.java
+++ GFB/src/LoginServlet.java
@@ -4,7 +4,8 @@
 // Author      : Ciaran McCormac
 // StudentId   : 87198584 
 // Version     : 1.0  -  30March21  -  Initial version
-//               1.1  -  03April21  -  Changed field names to match new SQL setup file
+//               1.1  -  03April21  -  Changed database field names to match new SQL setup file
+//               1.2  -  06April21  -  Changed form field names to match login.jsp. Added conditional user/admin launch
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import java.io.IOException;
@@ -39,25 +40,35 @@ public class LoginServlet extends HttpServlet
             // Connect to the database
             DatabaseConnection databaseConnection = new DatabaseConnection(sURL, sUsername, sPassword); 
 
-            String lName = request.getParameter("name");                                // Retrieve the form data
+            String lName = request.getParameter("uname");                                   // Retrieve the form data
             String lPass = request.getParameter("password");
 
-            int[] ret_vals = databaseConnection.authenticate(lName, lPass);             // Authenticate. If valid, returns the id and credentials (credentials=0 => User not found)
+            int[] ret_vals = databaseConnection.authenticate(lName, lPass);                 // Authenticate. If valid, returns the id and credentials (credentials=0 => User not found)
             lId          = ret_vals[0];
             lCredentials = ret_vals[1];
 
-            if (lCredentials != 0)                                                      // Non-zero credentials means user was found
+            if (lCredentials == 1)                                                          // Standard user
             {
-                System.out.println("Login Success");
-                request.getSession().setAttribute("id", lId);                           // Set the http session id          (for table cross-reference / key)
-                request.getSession().setAttribute("credentials", lCredentials);         // Set the http session credentials (for conditional menus)
-                request.getRequestDispatcher("landing.jsp").include(request, response); // Jump to the landing page
+                System.out.println("User Login Success");
+                request.getSession().setAttribute("id", lId);                               // Set the http session id
+                request.getSession().setAttribute("credentials", 1);                        // Set the http session credentials
+                request.getRequestDispatcher("accountPage.jsp").include(request, response); // Jump to the account page
+            }
+            else if (lCredentials == 2)                                                     // Admin user
+            {
+                System.out.println("Admin Login Success");
+                request.getSession().setAttribute("id", lId);                               // Set the http session id
+                request.getSession().setAttribute("credentials", 2);                        // Set the http session credentials
+                request.getRequestDispatcher("adminPage.jsp").include(request, response);   // Jump to the admin page
             }
             else
             {
                 System.out.println("Login Failure");
-                request.getRequestDispatcher("index.html").include(request, response);  // False means failed credential check, reenter details
+                request.getSession().setAttribute("id", 0);                                 // Blank the userId
+                request.getSession().setAttribute("credentials", 0);                        // Blank the http session credentials
+                request.getRequestDispatcher("login.jsp").include(request, response);       // Back to login page
             }
+
         } catch (Exception e) { e.printStackTrace(); }
     }
 
