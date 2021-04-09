9dae63cc9f9cf3cbec2d1c1fdffad8d50940235f
ciaranius <78697615+ciaranius@users.noreply.github.com>
2021-04-09 18:52:41 +0100

    Update LoginServlet.java
    Added database read of balance on login

--- GFB/src/LoginServlet.java
+++ GFB/src/LoginServlet.java
@@ -6,9 +6,13 @@
 // Version     : 1.0  -  30March21  -  Initial version
 //               1.1  -  03April21  -  Changed database field names to match new SQL setup file
 //               1.2  -  06April21  -  Changed form field names to match login.jsp. Added conditional user/admin launch
+//               1.3  -  09April21  -  Added database read of balance on login
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -40,35 +44,49 @@ public class LoginServlet extends HttpServlet
             // Connect to the database
             DatabaseConnection databaseConnection = new DatabaseConnection(sURL, sUsername, sPassword); 
 
-            String lName = request.getParameter("uname");                                   // Retrieve the form data
+            String lName = request.getParameter("lname");                                   // Retrieve the form data
             String lPass = request.getParameter("password");
 
             int[] ret_vals = databaseConnection.authenticate(lName, lPass);                 // Authenticate. If valid, returns the id and credentials (credentials=0 => User not found)
             lId          = ret_vals[0];
             lCredentials = ret_vals[1];
 
-            if (lCredentials == 1)                                                          // Standard user
-            {
-                System.out.println("User Login Success");
-                request.getSession().setAttribute("id", lId);                               // Set the http session id
-                request.getSession().setAttribute("credentials", 1);                        // Set the http session credentials
-                request.getRequestDispatcher("accountPage.jsp").include(request, response); // Jump to the account page
-            }
-            else if (lCredentials == 2)                                                     // Admin user
-            {
-                System.out.println("Admin Login Success");
-                request.getSession().setAttribute("id", lId);                               // Set the http session id
-                request.getSession().setAttribute("credentials", 2);                        // Set the http session credentials
-                request.getRequestDispatcher("adminPage.jsp").include(request, response);   // Jump to the admin page
-            }
-            else
+            if ((lCredentials != 1) && (lCredentials != 2))                                 // Bad credentials, login failure
             {
                 System.out.println("Login Failure");
                 request.getSession().setAttribute("id", 0);                                 // Blank the userId
-                request.getSession().setAttribute("credentials", 0);                        // Blank the http session credentials
+                request.getSession().setAttribute("credentials", 0);                        // Blank the credentials
                 request.getRequestDispatcher("login.jsp").include(request, response);       // Back to login page
+                return;
             }
 
+            // Read balance for immediate availability for display
+            String sqlStatement1 = "SELECT * FROM customer WHERE id = \"" + lId + "\"";
+            System.out.println("sqlStatement : " + sqlStatement1);
+            double lBalance = 0;
+            try
+            {
+                ResultSet resultSet1 = databaseConnection.statement.executeQuery(sqlStatement1);
+                if (resultSet1.next() == true)                      // Any query result means valid
+                	lBalance = resultSet1.getDouble("balance");
+            } catch (SQLException e) { e.printStackTrace(); }
+
+            if (lCredentials == 1)                                                          // Standard user
+            {
+                System.out.println("User Login Success");
+                request.getSession().setAttribute("id", lId);                               // Set the id
+                request.getSession().setAttribute("credentials", 1);                        // Set the credentials
+                request.getSession().setAttribute("balance", lBalance);                     // Set the balance
+                request.getRequestDispatcher("accountPage.jsp").include(request, response); // Jump to the account page
+            }
+            else                                                                            // Admin user
+            {
+                System.out.println("Admin Login Success");
+                request.getSession().setAttribute("id", lId);                               // Set the id
+                request.getSession().setAttribute("credentials", 2);                        // Set the credentials
+                request.getSession().setAttribute("balance", lBalance);                     // Set the balance
+                request.getRequestDispatcher("adminPage.jsp").include(request, response);   // Jump to the admin page
+            }
         } catch (Exception e) { e.printStackTrace(); }
     }
 
